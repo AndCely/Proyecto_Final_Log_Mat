86d5bf4a4b7f0340c60330cf404a3eeb60344f98
Santiago Andres Cely Rincon <santiago.andresce@gmail.com>
2024-11-15 18:42:43 -0500

    Agregue el archivo bullet.py y añadi codigo

--- Trabajo/settings.py
+++ Trabajo/settings.py
@@ -4,3 +4,8 @@ class Settings:
         self.screen_height = 700
         self.bg_color = (35, 35, 35)
         self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (211, 211, 211)
